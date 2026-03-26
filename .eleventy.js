3bc8899cdedaacf8000e79d5132d2e8918c4804f
Yuri Silva <yu.dev@icloud.com>
2026-03-26 17:20:18 +0000

    refact relatio website

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/robots.txt");
+  eleventyConfig.addPassthroughCopy("./src/sitemap.xml");
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+    },
+  };
+};
